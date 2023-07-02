5b50187a75d2f423b08393399656fc3070e29f49
salty-io <125950061+salty-io@users.noreply.github.com>
2023-07-02 13:12:30 -0800

    hopefully fixed pathspec error

--- stable/interfaces/IPriceFeed.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BSL 1.1
-pragma solidity ^0.8.12;
-
-
-interface IPriceFeed
-	{
-	function getPriceBTC() external view returns (uint256);
-	function getPriceETH() external view returns (uint256);
-	}
