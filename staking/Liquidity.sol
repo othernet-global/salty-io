b98fee99bc763d1110470886b7abad90e8f25c3d
salty-io <125950061+salty-io@users.noreply.github.com>
2023-07-09 13:26:29 -0800

    Update Liquidity.sol
    Unused tokens returned to  user

--- staking/Liquidity.sol
+++ staking/Liquidity.sol
@@ -11,7 +11,7 @@ import "../pools/interfaces/IPoolsConfig.sol";
 import "../pools/PoolUtils.sol";
 
 
-// Allows users to deposit and stake liquidity to receive a share of the rewards for that pool
+// Allows users to add liquidity and increase their liquidity share in the  StakingRewards pool so that they can receive proportional future rewards.
 contract Liquidity is ILiquidity, StakingRewards
     {
 	using SafeERC20 for IERC20;
@@ -28,55 +28,68 @@ contract Liquidity is ILiquidity, StakingRewards
 		}
 
 
-	// Add a certain amount of liquidity to the specified pool and stake the added liquidity within this contract for future rewards.
-	// Requires that the sending wallet has exchange access and that the pool is whitelisted
-	function addAndDepositLiquidity( IERC20 tokenA, IERC20 tokenB, uint256 maxAmountA, uint256 maxAmountB, uint256 minLiquidityReceived, uint256 deadline, bool bypassZapSwap ) public nonReentrant returns (uint256 addedAmountA, uint256 addedAmountB, uint256 addedLiquidity)
+	// Add a certain amount of liquidity to the specified pool and increase the user's liqudiity share for that pool so that they can receive future rewards.
+	// Tokens are zapped in by default - where all the tokens specified by the user are added to the liqudiity pool regardless of their ratio.
+	// With zapping, if one of the tokens has excess in regards to the reserves token ratio, then some of it is first swapped for the other before the liquidity is added. (See PoolMath.sol for details)
+	// bypassZapping allows this zapping to be avoided - which results in a simple addLiquidity call.
+	// Requires that the sending wallet has exchange access and that the pool is whitelisted (both checked in _increaseUserShare).
+	function addLiquidityAndIncreaseShare( IERC20 tokenA, IERC20 tokenB, uint256 maxAmountA, uint256 maxAmountB, uint256 minLiquidityReceived, uint256 deadline, bool bypassZapping ) public nonReentrant returns (uint256 addedAmountA, uint256 addedAmountB, uint256 addedLiquidity)
 		{
-		// Rememebr the initial underlying token balances so we can determine if any of the tokens remain after the liquidity is added
+		// Remember the initial underlying token balances so we can later determine if any of the user's tokens are later unused for adding liquidity and should be sent back.
 		uint256 startingBalanceA = tokenA.balanceOf( address(this) );
 		uint256 startingBalanceB = tokenB.balanceOf( address(this) );
 
-		// Transfer the maximum amount of tokens from the user
-		// Any extra underlying tokens after the liquidity is added will be sent back to the user
+		// Transfer the specified maximum amount of tokens from the user
 		tokenA.safeTransferFrom(msg.sender, address(this), maxAmountA );
 		tokenB.safeTransferFrom(msg.sender, address(this), maxAmountB );
 
-		// Zap in the specified liquidity (with the optional bypassZapSwap which will then just be a strict addLiquidity call)
+		// Zap in the specified liquidity (passing the specified bypassZapping as well).
+		// The added liquidity will be owned by this contract.
 		tokenA.approve( address(pools), maxAmountA );
 		tokenB.approve( address(pools), maxAmountB );
-		(addedAmountA, addedAmountB, addedLiquidity) = pools.dualZapInLiquidity( tokenA, tokenB, maxAmountA, maxAmountB, minLiquidityReceived, deadline, bypassZapSwap );
+		(addedAmountA, addedAmountB, addedLiquidity) = pools.dualZapInLiquidity( tokenA, tokenB, maxAmountA, maxAmountB, minLiquidityReceived, deadline, bypassZapping );
 
-		(bytes32 poolID,) = PoolUtils.poolID( tokenA, tokenB );
+		// Avoid stack too deep
+			{
+			(bytes32 poolID,) = PoolUtils.poolID( tokenA, tokenB );
 
-		// Update the user's share of the rewards for the pool (where the pool will be confirmed as whitelisted)
-		// Cooldown is specified to prevent reward hunting (quickly depositing and withdrawing liquidity to earn rewards)
-   		_increaseUserShare( msg.sender, poolID, addedLiquidity, true );
+			// Increase the user's liquidity share by the amount of addedLiquidity.
+			// Cooldown is specified to prevent reward hunting (ie - quickly depositing and withdrawing large amounts of liquidity to snipe rewards)
+			// Here the pool will be confirmed as whitelisted as well.
+			_increaseUserShare( msg.sender, poolID, addedLiquidity, true );
+			}
 
-		// Send any extra of the underlying tokens back to the user
-		uint256 extraBalanceA = startingBalanceA - tokenA.balanceOf( address(this) );
-		uint256 extraBalanceB = startingBalanceB - tokenB.balanceOf( address(this) );
+		// If any of the user's tokens were not used, then send them back
+		uint256 unusedTokensA = tokenA.balanceOf( address(this) ) - startingBalanceA;
+		if ( unusedTokensA > 0 )
+			tokenA.safeTransfer( msg.sender, unusedTokensA );
 
-		if ( extraBalanceA > 0 )
-			tokenA.transfer( msg.sender, extraBalanceA );
-		if ( extraBalanceB > 0 )
-			tokenB.transfer( msg.sender, extraBalanceB );
+		uint256 unusedTokensB = tokenB.balanceOf( address(this) ) - startingBalanceB;
+		if ( unusedTokensB > 0 )
+			tokenB.safeTransfer( msg.sender, unusedTokensB );
 		}
 
 
-	// Withdraw liquidity and claim any pending rewards.
-	// The DAO itself is not allowed to unstake liquidity
+	// Withdraw specified liquidity, decrease the user's liquidity share and claim any pending rewards.
+	// The DAO itself is not allowed to withdraw liquidity
      function withdrawLiquidityAndClaim( IERC20 tokenA, IERC20 tokenB, uint256 liquidityToWithdraw, uint256 minReclaimedA, uint256 minReclaimedB, uint256 deadline ) public nonReentrant returns (uint256 reclaimedA, uint256 reclaimedB)
 		{
 		// Make sure that the DAO isn't trying to remove liquidity
-		require( msg.sender != address(exchangeConfig.dao()), "DAO is not allowed to unstake LP tokens" );
+		require( msg.sender != address(exchangeConfig.dao()), "DAO is not allowed to withdraw liquidity" );
 
 		(bytes32 poolID,) = PoolUtils.poolID( tokenA, tokenB );
 
-		// Update the user's share of the rewards for the specified pool
-		// Note: _decreaseUserShare checks to make sure the user has the specified share
+		// Reduce the user's liqudiity share for the specified pool so that they receive less rewards.
+		// Cooldown is specified to prevent reward hunting (ie - quickly depositing and withdrawing large amounts of liquidity to snipe rewards)
+		// This call will send any pending SALT rewards to msg.sender as well.
+		// Note: _decreaseUserShare checks to make sure that the user has the specified liquidity share.
 		_decreaseUserShare( msg.sender, poolID, liquidityToWithdraw, true );
 
 		// Remove the amount of liquidity specified by the user
 		(reclaimedA, reclaimedB) = pools.removeLiquidity( tokenA, tokenB, liquidityToWithdraw, minReclaimedA, minReclaimedB, deadline );
+
+		// Transfer the reclaimed tokens to the user
+		tokenA.safeTransfer( msg.sender, reclaimedA );
+		tokenB.safeTransfer( msg.sender, reclaimedB );
 		}
 	}
