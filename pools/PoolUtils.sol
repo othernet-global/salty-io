ddfe12b961c3605b80fb368d954844014ee46ed0
salty-io <125950061+salty-io@users.noreply.github.com>
2023-07-11 15:59:28 -0800

    Added checkRatiosAreSimilar

--- pools/PoolUtils.sol
+++ pools/PoolUtils.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.8.12;
 
 import "../openzeppelin/token/ERC20/IERC20.sol";
+import "../openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
 
 library PoolUtils
 	{
+	using SafeERC20 for IERC20;
+
     // Return the unique poolID for the given two tokens.
     // Tokens are sorted before being hashed to make reversed pairs equivalent.
     // flipped = address(tokenB) < address(tokenA)
@@ -16,4 +19,22 @@ library PoolUtils
 
         return (keccak256(abi.encodePacked(address(tokenA), address(tokenB))), false);
     	}
+
+
+    // Returns true if the percentage difference between A/B and C/D is less than or equal to maxPercentDifferenceTimes1000
+    function checkRatiosAreSimilar(uint256 A, uint256 B, uint256 C, uint256 D, uint256 maxPercentDifferenceTimes1000 ) internal pure returns (bool)
+    	{
+    	// Zero denominators aren't meaningful
+    	if ( ( B==0 ) || ( D==0 ) )
+    		return false;
+
+		uint256 ratio1 = A * D;
+		uint256 ratio2 = B * C;
+
+		// Make sure the larger of the ratios isn't outside the specified limit
+		if ( ratio1 > ratio2 )
+			return ( ratio1 <= ratio2 * (100000 + maxPercentDifferenceTimes1000) / 100000);
+
+		return ( ratio2 <= ratio1 * (100000 + maxPercentDifferenceTimes1000) / 100000);
+        }
 	}
