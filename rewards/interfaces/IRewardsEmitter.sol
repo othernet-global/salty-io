a4d5727d3682b903fe67d71061d0ac3a2d2ce739
salty-io <125950061+salty-io@users.noreply.github.com>
2023-07-11 16:04:28 -0800

    Streamlined interfaces

--- rewards/interfaces/IRewardsEmitter.sol
+++ rewards/interfaces/IRewardsEmitter.sol
@@ -4,21 +4,14 @@ pragma solidity ^0.8.12;
 import "../../staking/interfaces/IStakingRewards.sol";
 import "../../staking/interfaces/IStakingConfig.sol";
 import "../../pools/interfaces/IPoolsConfig.sol";
-import "../../interfaces/IUpkeepable.sol";
 import "../../interfaces/IExchangeConfig.sol";
 import "./IRewardsConfig.sol";
 
 
-interface IRewardsEmitter is IUpkeepable
+interface IRewardsEmitter
 	{
 	function addSALTRewards( AddedReward[] calldata addedRewards ) external;
 
 	// Views
-	function exchangeConfig() external view returns (IExchangeConfig);
-	function poolsConfig() external view returns (IPoolsConfig);
-	function rewardsConfig() external view returns (IRewardsConfig);
-	function stakingConfig() external view returns (IStakingConfig);
-	function stakingRewards() external view returns (IStakingRewards);
-
 	function pendingRewardsForPools( bytes32[] calldata pairs ) external view returns (uint256[] calldata);
 	}
