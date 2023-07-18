1967490bf08d68b71e48c227dcb830aadf22d043
salty-io <125950061+salty-io@users.noreply.github.com>
2023-07-18 00:33:10 -0800

    Update IExchangeConfig.sol

--- interfaces/IExchangeConfig.sol
+++ interfaces/IExchangeConfig.sol
@@ -2,19 +2,21 @@
 pragma solidity ^0.8.12;
 
 import "./IAAA.sol";
-import "./IPOL_Optimizer.sol";
 import "../dao/interfaces/IDAO.sol";
 import "../interfaces/IAccessManager.sol";
 import "../stable/interfaces/IUSDS.sol";
 import "../interfaces/ISalt.sol";
-
+import "../rewards/interfaces/IRewardsEmitter.sol";
+import "../rewards/interfaces/IRewardsConfig.sol";
 
 interface IExchangeConfig
 	{
 	function setDAO( IDAO _dao ) external; // onlyOwner
 	function setAAA( IAAA _aaa ) external; // onlyOwner
 	function setAccessManager( IAccessManager _accessManager ) external; // onlyOwner
-	function setOptimizer( IPOL_Optimizer _optimizer ) external; // onlyOwner
+	function setStakingRewardsEmitter( IRewardsEmitter _rewardsEmitter ) external; // onlyOwner
+	function setLiquidityRewardsEmitter( IRewardsEmitter _rewardsEmitter ) external; // onlyOwner
+	function setCollateralRewardsEmitter( IRewardsEmitter _rewardsEmitter ) external; // onlyOwner
 
 	// Views
 	function salt() external view returns (ISalt);
@@ -26,7 +28,9 @@ interface IExchangeConfig
 	function aaa() external view returns (IAAA);
 	function accessManager() external view returns (IAccessManager);
 	function dao() external view returns (IDAO);
-	function optimizer() external view returns (IPOL_Optimizer);
+	function stakingRewardsEmitter() external view returns (IRewardsEmitter);
+	function liquidityRewardsEmitter() external view returns (IRewardsEmitter);
+	function collateralRewardsEmitter() external view returns (IRewardsEmitter);
 
 	function walletHasAccess( address wallet ) external view returns (bool);
 	}
