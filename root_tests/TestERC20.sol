f65b26b5f1ae58e53b3cc300adffc06502b4c2c7
salty-io <125950061+salty-io@users.noreply.github.com>
2023-07-18 00:33:16 -0800

    Update TestERC20.sol

--- root_tests/TestERC20.sol
+++ root_tests/TestERC20.sol
@@ -6,34 +6,21 @@ import "../openzeppelin/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20
     {
-    uint8 private _decimals = 18;
+    uint8 private _decimals = 6;
 
 	uint256 public INITIAL_SUPPLY;
 
 
 
-	constructor( uint8 __decimals )
-		ERC20( "Test", "TEST" )
+	constructor( uint256 __decimals )
+		ERC20( "TestUSDC", "USDC" )
 		{
-		_decimals = __decimals;
+		_decimals = uint8(__decimals);
 
 		// 1 trillion initial supply
 		INITIAL_SUPPLY = 1000 * 1000000000 * 10 ** _decimals;
 		_mint( msg.sender, INITIAL_SUPPLY );
         }
-
-
-	// So the token can pass the whitelisting validation
-    function kLast() public pure returns (uint)
-    	{
-    	return 0;
-    	}
-
-
-    function getReserves() public pure returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)
-		{
-		return (0, 0, 0 );
-		}
 
 
     function decimals() public view override returns (uint8)
