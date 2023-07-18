3e18f9f6fe6b4167a8788ecdfac5505e325d3834
salty-io <125950061+salty-io@users.noreply.github.com>
2023-07-18 00:31:33 -0800

    Moved files

--- interfaces/IPOL_Optimizer.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BSL 1.1
-pragma solidity ^0.8.12;
-
-
-interface IPOL_Optimizer
-	{
-	function lastSwapTimestamp( address token0, address token1 ) external view returns (uint32);
-	function findBestPool() external view returns (uint256 maxPendingReward, bytes32 bestPool);
-	}
