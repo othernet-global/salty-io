fc8b1360a6a5c1fcb662b04b4c60dfad14470400
salty-io <125950061+salty-io@users.noreply.github.com>
2023-07-19 14:28:48 -0800

    Update DAO.sol

--- dao/DAO.sol
+++ dao/DAO.sol
@@ -69,21 +69,6 @@ contract DAO is IDAO, Upkeepable, Parameters
         }
 
 
-	// Performs upkeep on the exchange, handling various housekeeping functions such as:
-	// Emissions - distributing SALT rewards to the stakingRewardsEmitter and liquidityRewardsEmitter
-	// AAA - converting previous arbitrage profits from WETH to SALT and sending them to the releveant RewardsEmitters
-	// RewardsEmitters - for staking, liquidity and collateral SALT rewards distribution.
-	// Liquidator - liquidating any LP that is currently being held in the Liquidator contract, burning the required amount of USDS and sending extra WETH to the POL_Optimizer.
-	// POL_Optimizer - forming optimized Protocol Owned Liquidity with the WETH it has been sent.
-	// DAO - staking any LP that was sent to it by the POL_Optimizer.
-
-	// The caller of performUpkeep receives a share of the DAO Protocol Owned Liquidity profits that are claimed during the upkeep and also
-	// receives any WETH (swapped to SALT) that was sent by the AAA on its performUpkeep.
-	function _performUpkeep() internal override
-		{
-		}
-
-
 	// Finalize the vote for a parameter ballot (increase, decrease or no_change) for a given parameter
 	function _finalizeParameterBallot( uint256 ballotID ) internal
 		{
@@ -230,6 +215,23 @@ contract DAO is IDAO, Upkeepable, Parameters
 			_finalizeTokenWhitelisting(ballotID);
 		else
 			_finalizeApprovalBallot(ballotID);
+		}
+
+
+
+
+	// Performs upkeep on the exchange, handling various housekeeping functions such as:
+	// Emissions - distributing SALT rewards to the stakingRewardsEmitter and liquidityRewardsEmitter
+	// AAA - converting previous arbitrage profits from WETH to SALT and sending them to the releveant RewardsEmitters
+	// RewardsEmitters - for staking, liquidity and collateral SALT rewards distribution.
+	// Liquidator - liquidating any LP that is currently being held in the Liquidator contract, burning the required amount of USDS and sending extra WETH to the POL_Optimizer.
+	// POL_Optimizer - forming optimized Protocol Owned Liquidity with the WETH it has been sent.
+	// DAO - staking any LP that was sent to it by the POL_Optimizer.
+
+	// The caller of performUpkeep receives a share of the DAO Protocol Owned Liquidity profits that are claimed during the upkeep and also
+	// receives any WETH (swapped to SALT) that was sent by the AAA on its performUpkeep.
+	function _performUpkeep() internal override
+		{
 		}
 
 
