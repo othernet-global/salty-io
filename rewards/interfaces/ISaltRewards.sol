3f627bc8260f588bfa9f488f31b68c122958cae4
salty-io <125950061+salty-io@users.noreply.github.com>
2023-09-06 15:50:36 -0800

    Initial implementation

--- rewards/interfaces/ISaltRewards.sol
+++ rewards/interfaces/ISaltRewards.sol
@@ -5,5 +5,6 @@ pragma solidity =0.8.21;
 interface ISaltRewards
 	{
 	function addSALTRewards(uint256 amount) external;
-	function performUpkeep( bytes32[] calldata poolIDs, uint256[] calldata profitsForPools ) external;
+	function sendInitialSaltRewards( uint256 liquidityBootstrapAmount, uint256 stakingBootstrapAmount, bytes32[] memory poolIDs ) external;
+    function performUpkeep( bytes32[] calldata poolIDs, uint256[] calldata profitsForPools ) external;
     }
