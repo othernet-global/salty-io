af1fd4163506236a4bd6e52ef2ab03b3b350c455
salty-io <125950061+salty-io@users.noreply.github.com>
2023-10-11 19:31:15 -0800

    Updated comments

--- AccessManager.sol
+++ AccessManager.sol
@@ -5,14 +5,17 @@ import "./interfaces/IAccessManager.sol";
 import "./dao/interfaces/IDAO.sol";
 
 
-// A simple AccessManager in which a user is confirmed to have access offchain in the browser and then verified user status is stored in the contract.
-// When geographic regions are later excluded, users are required to reverify (allowing avoiding storing countries for specific wallets and potentially violating user privacy).
+// A simple AccessManager in which user IP is mapped offchain to a geolocation and then whitelisted user status is stored in the contract.
+// If geographic regions are later excluded, users are required to reverify (allowing avoiding storing countries for specific wallets and potentially violating user privacy).
+// The AccessManager restricts users from adding liquidity, adding collateral and borrowing USDS on the contract level - but always allows existing assets to be removed (in case a user's region is restricted after depositing assets).
 //
-// Note that using ExchangeConfig.setAccessManager() this contract can be replaced by the DAO with orther mechanics such as decentralized ID services, KYC by region, or whatever else deemed necessary by the DAO to satisfy the changing regulatory landscape.
-// The AccessManager restricts users from swapping, making DAO proposals, voting, adding liquidity, adding collateral and borrowing USDS on the contract level - but always allows existing assets to be removed (in case a user's region is restricted after depositing assets).
-// DAO interaction is not restricted on the contract level - in the case that the AccessManager mistakingly denies everyone access which woudl prevent it from being replaced by the DAO.
+// This contract can be replaced by the DAO with other mechanics such as decentralized ID services, KYC by region, or whatever else deemed necessary by the DAO to satisfy the changing regulatory landscape.
+//
+// Making proposals and voting is not access restricted - just in case there is ever AccessManager.sol is ever updated with a flaw in it that universally prevents access (which would effectively cripple the DAO if proposals and voting were then mistakingly resricted).
+// Acquiring xSALT requires access though as the underlying staking mechanism requires access.
 //
 // Updateable using DAO.proposeSetContractAddress( "accessManager" )
+
 contract AccessManager is IAccessManager
 	{
 	address constant public EXPECTED_SIGNER = 0x1234519DCA2ef23207E1CA7fd70b96f281893bAa;
