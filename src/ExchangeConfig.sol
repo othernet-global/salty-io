cb1ab69ff239c4569341b3be0da2e61a7d519cc3
salty-io <125950061+salty-io@users.noreply.github.com>
2023-11-15 13:22:40 -0800

    setContracts used instead of multiple individual functions to set each contract
    rewardsEmitters no longer modifiable by the DAO after deployment

--- src/ExchangeConfig.sol
+++ src/ExchangeConfig.sol
@@ -2,153 +2,96 @@
 pragma solidity =0.8.22;
 
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
-import "./interfaces/IExchangeConfig.sol";
-import "./rewards/interfaces/IRewardsEmitter.sol";
-import "./interfaces/IUpkeep.sol";
 import "./launch/interfaces/IInitialDistribution.sol";
+import "./rewards/interfaces/IRewardsEmitter.sol";
+import "./interfaces/IExchangeConfig.sol";
 import "./launch/interfaces/IAirdrop.sol";
-
+import "./interfaces/IUpkeep.sol";
+import "./interfaces/IManagedWallet.sol";
 
 // Contract owned by the DAO with parameters modifiable only by the DAO
 contract ExchangeConfig is IExchangeConfig, Ownable
     {
+    event AccessManagerSet(IAccessManager indexed accessManager);
+
 	ISalt immutable public salt;
 	IERC20 immutable public wbtc;
 	IERC20 immutable public weth;
 	IERC20 immutable public dai;
 	IUSDS immutable public usds;
+	IManagedWallet immutable public managedTeamWallet;
 
 	IDAO public dao; // can only be set once
 	IUpkeep public upkeep; // can only be set once
-
-	IRewardsEmitter public stakingRewardsEmitter;
-	IRewardsEmitter public liquidityRewardsEmitter;
-	IAccessManager public accessManager;
-	IInitialDistribution public initialDistribution;
-	IAirdrop public airdrop;
-
-	address public teamWallet;
-	address public proposedTeamWallet;
+	IInitialDistribution public initialDistribution; // can only be set once
+	IAirdrop public airdrop; // can only be set once
 
 	// Gradually distribute SALT to the teamWallet and DAO over 10 years
-	address public teamVestingWallet;	// can only be set once
-	address public daoVestingWallet;		// can only be set once
+	VestingWallet public teamVestingWallet;		// can only be set once
+	VestingWallet public daoVestingWallet;		// can only be set once
+
+	IAccessManager public accessManager;
 
 
-	constructor( ISalt _salt, IERC20 _wbtc, IERC20 _weth, IERC20 _dai, IUSDS _usds, address _teamWallet )
+	constructor( ISalt _salt, IERC20 _wbtc, IERC20 _weth, IERC20 _dai, IUSDS _usds, IManagedWallet _managedTeamWallet )
 		{
 		require( address(_salt) != address(0), "_salt cannot be address(0)" );
 		require( address(_wbtc) != address(0), "_wbtc cannot be address(0)" );
 		require( address(_weth) != address(0), "_weth cannot be address(0)" );
 		require( address(_dai) != address(0), "_dai cannot be address(0)" );
 		require( address(_usds) != address(0), "_usds cannot be address(0)" );
-		require( address(_teamWallet) != address(0), "_teamWallet cannot be address(0)" );
+		require( address(_managedTeamWallet) != address(0), "_managedTeamWallet cannot be address(0)" );
 
 		salt = _salt;
 		wbtc = _wbtc;
 		weth = _weth;
 		dai = _dai;
 		usds = _usds;
-		teamWallet = _teamWallet;
+		managedTeamWallet = _managedTeamWallet;
         }
 
 
-	function setDAO( IDAO _dao ) public onlyOwner
+	// setContracts can only be be called once - and is called at deployment time.
+	function setContracts( IDAO _dao, IUpkeep _upkeep, IInitialDistribution _initialDistribution, IAirdrop _airdrop, VestingWallet _teamVestingWallet, VestingWallet _daoVestingWallet ) external onlyOwner
 		{
-		require( address(dao) == address(0), "setDAO can only be called once" );
+		// setContracts is only called once (on deployment)
+		require( address(dao) == address(0), "setContracts can only be called once" );
+
 		require( address(_dao) != address(0), "_dao cannot be address(0)" );
-
-		dao = _dao;
-		}
-
-
-	function setUpkeep( IUpkeep _upkeep ) public onlyOwner
-		{
-		require( address(upkeep) == address(0), "setUpkeep can only be called once" );
 		require( address(_upkeep) != address(0), "_upkeep cannot be address(0)" );
-
-		upkeep = _upkeep;
-		}
-
-
-	function setVestingWallets( address _teamVestingWallet, address _daoVestingWallet ) public onlyOwner
-		{
-		require( address(teamVestingWallet) == address(0), "setVestingWallets can only be called once" );
+		require( address(_initialDistribution) != address(0), "_initialDistribution cannot be address(0)" );
+		require( address(_airdrop) != address(0), "_airdrop cannot be address(0)" );
 		require( address(_teamVestingWallet) != address(0), "_teamVestingWallet cannot be address(0)" );
 		require( address(_daoVestingWallet) != address(0), "_daoVestingWallet cannot be address(0)" );
 
+		dao = _dao;
+		upkeep = _upkeep;
+		initialDistribution = _initialDistribution;
+		airdrop = _airdrop;
 		teamVestingWallet = _teamVestingWallet;
 		daoVestingWallet = _daoVestingWallet;
 		}
 
 
-	function setInitialDistribution( IInitialDistribution _initialDistribution ) public onlyOwner
-		{
-		require( address(initialDistribution) == address(0), "setInitialDistribution can only be called once" );
-		require( address(_initialDistribution) != address(0), "_initialDistribution cannot be address(0)" );
-
-		initialDistribution = _initialDistribution;
-		}
-
-
-	function setAirdrop( IAirdrop _airdrop ) public onlyOwner
-		{
-		require( address(airdrop) == address(0), "setAirdrop can only be called once" );
-		require( address(_airdrop) != address(0), "_airdrop cannot be address(0)" );
-
-		airdrop = _airdrop;
-		}
-
-
-	function setAccessManager( IAccessManager _accessManager ) public onlyOwner
+	function setAccessManager( IAccessManager _accessManager ) external onlyOwner
 		{
 		require( address(_accessManager) != address(0), "_accessManager cannot be address(0)" );
 
 		accessManager = _accessManager;
-		}
 
-
-	function setStakingRewardsEmitter( IRewardsEmitter _rewardsEmitter ) public onlyOwner
-		{
-		require( address(_rewardsEmitter) != address(0), "_rewardsEmitter cannot be address(0)" );
-
-		stakingRewardsEmitter = _rewardsEmitter;
-		}
-
-
-	function setLiquidityRewardsEmitter( IRewardsEmitter _rewardsEmitter ) public onlyOwner
-		{
-		require( address(_rewardsEmitter) != address(0), "_rewardsEmitter cannot be address(0)" );
-
-		liquidityRewardsEmitter = _rewardsEmitter;
-		}
-
-
-	function proposeTeamWallet( address _teamWallet ) public
-		{
-		require( msg.sender == teamWallet, "Only the current team can change the teamWallet" );
-
-		proposedTeamWallet = _teamWallet;
-		}
-
-
-	function confirmTeamWallet() public
-		{
-		require( msg.sender == proposedTeamWallet, "Only the proposed teamWallet can confirm the teamWallet change" );
-
-		teamWallet = proposedTeamWallet;
+	    emit AccessManagerSet(_accessManager);
 		}
 
 
 	// Provide access to the protocol components using the AccessManager to determine if a wallet should have access.
 	// AccessManager can be updated by the DAO and include any necessary functionality.
-	function walletHasAccess( address wallet ) public view returns (bool)
+	function walletHasAccess( address wallet ) external view returns (bool)
 		{
-		// The DAO contract always has access
+		// The DAO contract always has access (needed to form POL)
 		if ( wallet == address(dao) )
 			return true;
 
-		// The Airdrop contract always has access
+		// The Airdrop contract always has access (needed to stake SALT)
 		if ( wallet == address(airdrop) )
 			return true;
 
--- src/root_tests/ExchangeConfig.t.sol
+++ src/root_tests/ExchangeConfig.t.sol
@@ -33,201 +33,45 @@ contract TestExchangeConfig is Deployment
 
     		// Test with _salt as zero address
     		vm.expectRevert("_salt cannot be address(0)");
-    		new ExchangeConfig(ISalt(address(0)), fakeIERC20, fakeIERC20, fakeIERC20, fakeUSDS, alice);
+    		new ExchangeConfig(ISalt(address(0)), fakeIERC20, fakeIERC20, fakeIERC20, fakeUSDS, managedTeamWallet);
 
     		// Test with _wbtc as zero address
     		vm.expectRevert("_wbtc cannot be address(0)");
-    		new ExchangeConfig(salt, IERC20(address(0)), fakeIERC20, fakeIERC20, fakeUSDS, alice);
+    		new ExchangeConfig(salt, IERC20(address(0)), fakeIERC20, fakeIERC20, fakeUSDS, managedTeamWallet);
 
     		// Test with _weth as zero address
     		vm.expectRevert("_weth cannot be address(0)");
-    		new ExchangeConfig(salt, fakeIERC20, IERC20(address(0)), fakeIERC20, fakeUSDS, alice);
+    		new ExchangeConfig(salt, fakeIERC20, IERC20(address(0)), fakeIERC20, fakeUSDS, managedTeamWallet);
 
     		// Test with _dai as zero address
     		vm.expectRevert("_dai cannot be address(0)");
-    		new ExchangeConfig(salt, fakeIERC20, fakeIERC20, IERC20(address(0)), fakeUSDS, alice);
+    		new ExchangeConfig(salt, fakeIERC20, fakeIERC20, IERC20(address(0)), fakeUSDS, managedTeamWallet);
 
     		// Test with _usds as zero address
     		vm.expectRevert("_usds cannot be address(0)");
-    		new ExchangeConfig(salt, fakeIERC20, fakeIERC20, fakeIERC20, IUSDS(address(0)), alice);
+    		new ExchangeConfig(salt, fakeIERC20, fakeIERC20, fakeIERC20, IUSDS(address(0)), managedTeamWallet);
 
     		// Test with _teamWallet as zero address
-    		vm.expectRevert("_teamWallet cannot be address(0)");
-    		new ExchangeConfig(salt, fakeIERC20, fakeIERC20, fakeIERC20, fakeUSDS, address(0));
+    		vm.expectRevert("_managedTeamWallet cannot be address(0)");
+    		new ExchangeConfig(salt, fakeIERC20, fakeIERC20, fakeIERC20, fakeUSDS, IManagedWallet(address(0)));
     		}
-
-
-    // A unit test to check the setDAO function when it is called by an address other than the owner. Verify that the function reverts with the correct error message.
-	function testSetDAOAsNonOwner() public
-        {
-        vm.prank(DEPLOYER);
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-		vm.expectRevert("Ownable: caller is not the owner" );
-		exchangeConfig.setDAO(IDAO(address(0x2222)));
-        }
-
-
-    // A unit test to check the setDAO function when called by the owner and the dao state variable has not been set yet. Ensure that the dao state variable is correctly set.
-    function testSetDAO() public
-        {
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-        // Initialize an instance of the DAO
-        IDAO _dao = IDAO(address(0x1111));
-
-        // Call `setDAO` function with the initialized _dao instance
-        exchangeConfig.setDAO(_dao);
-
-        // Assert that the `dao` state variable is now equal to the _dao instance
-        assertEq( address(exchangeConfig.dao()), address(_dao) );
-
-        // Calling `setDAO` again should throw an error
-        vm.expectRevert("setDAO can only be called once");
-        exchangeConfig.setDAO(_dao);
-        }
-
-
-    // A unit test to check the setUpkeep function when it is called by an address other than the owner. Verify that the function reverts with the correct error message.
-	function testSetUpkeepAsNonOwner() public
-        {
-        vm.prank(DEPLOYER);
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-		vm.expectRevert("Ownable: caller is not the owner" );
-		exchangeConfig.setUpkeep(IUpkeep(address(0x2222)));
-        }
-
-
-    // A unit test to check the setUpkeep function when called by the owner and the upkeep state variable has not been set yet. Ensure that the upkeep state variable is correctly set.
-    function testSetUpkeep() public
-        {
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-        // Initialize an instance of Upkeep
-        IUpkeep _upkeep = IUpkeep(address(0x1111));
-
-        // Call `setUpkeep` function with the initialized _upkeep instance
-        exchangeConfig.setUpkeep(_upkeep);
-
-        // Assert that the `upkeep` state variable is now equal to the _upkeep instance
-        assertEq( address(exchangeConfig.upkeep()), address(_upkeep) );
-
-        // Calling `setUpkeep` again should throw an error
-        vm.expectRevert("setUpkeep can only be called once");
-        exchangeConfig.setUpkeep(_upkeep);
-        }
-
-
-
-    // A unit test to check the setVestingWallets function when it is called by an address other than the owner. Verify that the function reverts with the correct error message.
-	function testSetVestingWalletsAsNonOwner() public
-        {
-        vm.prank(DEPLOYER);
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-		vm.expectRevert("Ownable: caller is not the owner" );
-		exchangeConfig.setVestingWallets(address(0x2222), address(0x2223));
-        }
-
-
-    // A unit test to check the setInitialDistribution function when it is called by an address other than the owner. Verify that the function reverts with the correct error message.
-	function testSetInitialDistributionAsNonOwner() public
-        {
-        vm.prank(DEPLOYER);
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-		vm.expectRevert("Ownable: caller is not the owner" );
-		exchangeConfig.setInitialDistribution(IInitialDistribution(address(0x2222)));
-        }
-
 
 
     // A unit test to check the setAccessManager function when it is called by an address other than the owner. Verify that the function reverts with the correct error message.
 	function testSetAccessManagerAsNonOwner() public
         {
         vm.prank(DEPLOYER);
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
+        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, managedTeamWallet);
 
 		vm.expectRevert("Ownable: caller is not the owner" );
 		exchangeConfig.setAccessManager(IAccessManager(address(0x2222)));
         }
 
 
-
-    // A unit test to check the setStakingRewardsEmitter function when it is called by an address other than the owner. Verify that the function reverts with the correct error message.
-	function testSetStakingRewardsEmitterAsNonOwner() public
-        {
-        vm.prank(DEPLOYER);
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-		vm.expectRevert("Ownable: caller is not the owner" );
-		exchangeConfig.setStakingRewardsEmitter(IRewardsEmitter(address(0x2222)));
-        }
-
-
-    // A unit test to check the setLiquidityRewardsEmitter function when it is called by an address other than the owner. Verify that the function reverts with the correct error message.
-	function testSetLiquidityRewardsEmitterAsNonOwner() public
-        {
-        vm.prank(DEPLOYER);
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-		vm.expectRevert("Ownable: caller is not the owner" );
-		exchangeConfig.setLiquidityRewardsEmitter(IRewardsEmitter(address(0x2222)));
-        }
-
-
-
-
-
-
-
-
-
-    // A unit test to check the setVestingWallets function when called by the owner and the vestingWallets state variables have not been set yet. Ensure that the vestingWallets state variables are correctly set.
-    function testSetVestingWallets() public
-    {
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-        // Addresses for vesting wallets
-        address teamVestingWalletAddr = address(0x1111);
-        address daoVestingWalletAddr = address(0x1112);
-
-        // Call `setVestingWallets` function with the initialized addresses
-        exchangeConfig.setVestingWallets(teamVestingWalletAddr, daoVestingWalletAddr);
-
-        // Assert that the `daoVestingWallet` and `teamVestingWallet` state variable are now equal to the initialized addresses
-        assertEq(exchangeConfig.daoVestingWallet(), daoVestingWalletAddr);
-        assertEq(exchangeConfig.teamVestingWallet(), teamVestingWalletAddr);
-
-        // Calling `setVestingWallets` again should throw an error
-        vm.expectRevert("setVestingWallets can only be called once");
-        exchangeConfig.setVestingWallets(address(0x2222), address(0x2223));
-    }
-
-    // A unit test to check the setInitialDistribution function when called by the owner and the initialDistribution state variable has not been set yet. Ensure that the initialDistribution state variable is correctly set.
-    function testSetInitialDistribution() public
-    {
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-        // Initialize an instance of the InitialDistribution
-        IInitialDistribution _initDist = IInitialDistribution(address(0x1111));
-
-        // Call `setInitialDistribution` function with the initialized _initDist instance
-        exchangeConfig.setInitialDistribution(_initDist);
-
-        // Assert that the `initialDistribution` state variable is now equal to the _initDist instance
-        assertEq(address(exchangeConfig.initialDistribution()), address(_initDist));
-
-        // Calling `setInitialDistribution` again should throw an error
-        vm.expectRevert("setInitialDistribution can only be called once");
-        exchangeConfig.setInitialDistribution(_initDist);
-    }
-
     // A unit test to check the setAccessManager function when called by the owner and the accessManager state variable has not been set yet. Ensure that the accessManager state variable is correctly set.
     function testSetAccessManager() public
     {
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
+        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, managedTeamWallet);
 
         // Initialize an instance of the AccessManager
         IAccessManager _accessManager = IAccessManager(address(0x1111));
@@ -237,62 +81,6 @@ contract TestExchangeConfig is Deployment
 
         // Assert that the `accessManager` state variable is now equal to the _accessManager instance
         assertEq(address(exchangeConfig.accessManager()), address(_accessManager));
-    }
-
-    // A unit test to check the setStakingRewardsEmitter function when called by the owner and the stakingRewardsEmitter state variable has not been set yet. Ensure that the stakingRewardsEmitter state variable is correctly set.
-    function testSetStakingRewardsEmitter() public
-    {
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-        // Initialize an instance of the StakingRewardsEmitter
-        IRewardsEmitter _rewardsEmitter = IRewardsEmitter(address(0x1111));
-
-        // Call `setStakingRewardsEmitter` function with the initialized _rewardsEmitter instance
-        exchangeConfig.setStakingRewardsEmitter(_rewardsEmitter);
-
-        // Assert that the `stakingRewardsEmitter` state variable is now equal to the _rewardsEmitter instance
-        assertEq(address(exchangeConfig.stakingRewardsEmitter()), address(_rewardsEmitter));
-    }
-
-    // A unit test to check the setLiquidityRewardsEmitter function when called by the owner and the liquidityRewardsEmitter state variable has not been set yet. Ensure that the liquidityRewardsEmitter state variable is correctly set.
-    function testSetLiquidityRewardsEmitter() public
-    {
-        exchangeConfig = new ExchangeConfig(salt, wbtc, weth, dai, usds, teamWallet);
-
-        // Initialize an instance of the StakingRewardsEmitter
-        IRewardsEmitter _rewardsEmitter = IRewardsEmitter(address(0x1111));
-
-        // Call `setLiquidityRewardsEmitter` function with the initialized _rewardsEmitter instance
-        exchangeConfig.setLiquidityRewardsEmitter(_rewardsEmitter);
-
-        // Assert that the `liquidityRewardsEmitter` state variable is now equal to the _rewardsEmitter instance
-        assertEq(address(exchangeConfig.liquidityRewardsEmitter()), address(_rewardsEmitter));
-    }
-
-
-    // A unit test to test that proposeTeamWallet works as expected
-    function testProposeTeamWallet() public
-    	{
-        // Initialize a random address
-        address randomAddr = address(0x1234);
-
-        // Update teamWallet to a random address
-        vm.prank(teamWallet);
-        exchangeConfig.proposeTeamWallet(randomAddr);
-
-        // Assert that the teamWallet state variable is now equal to the random address
-        assertEq(exchangeConfig.proposedTeamWallet(), randomAddr);
-    }
-
-
-    // A unit test to check the setTeamWallet function when it is called by an address other than the current teamWallet. Verify that the function reverts with the correct error message.
-    function testSetTeamWalletCalledByOtherThanTeamWallet() public {
-        // Call function with Prank
-        vm.prank(address(0x2345));
-
-        // Expect function to revert with correct message
-        vm.expectRevert("Only the current team can change the teamWallet");
-        exchangeConfig.proposeTeamWallet(address(0x1234));
     }
 
 
@@ -308,7 +96,7 @@ contract TestExchangeConfig is Deployment
     function testWalletHasAccessNonDAOWalletAllowedByAccessManager() public {
 
     	// Give access to the user
-		bytes memory sig = abi.encodePacked(hex"140e05ee4a808d731d7a8d72d685f7335c13b502823a4ff504fb894b96b230a277a693cc4d7ad68065776e05d9b7159fdb193912611d3c3c39abffe6f0e73bbf1c");
+		bytes memory sig = abi.encodePacked(oneTwoThreeFourAccessSignature);
 		vm.prank( address(0x1234) );
 		accessManager.grantAccess(sig);
 
@@ -323,27 +111,4 @@ contract TestExchangeConfig is Deployment
     	// Call `walletHasAccess` function with a non-DAO address which is not allowed by accessManager
     	assertFalse(exchangeConfig.walletHasAccess(address(0x1234)));
     }
-
-
-    // A unit test to test that confirmTeamWallet works as expected
-    function testConfirmTeamWallet() public
-    	{
-        // Initialize a random address
-        address randomAddr = address(0x1234);
-
-        // Update teamWallet to a random address
-        vm.prank(teamWallet);
-        exchangeConfig.proposeTeamWallet(randomAddr);
-
-        assertEq(exchangeConfig.proposedTeamWallet(), randomAddr);
-
-		vm.expectRevert( "Only the proposed teamWallet can confirm the teamWallet change" );
-		exchangeConfig.confirmTeamWallet();
-
-        vm.prank(randomAddr);
-        exchangeConfig.confirmTeamWallet();
-
-        // Assert that the teamWallet state variable is now equal to the random address
-        assertEq(exchangeConfig.teamWallet(), randomAddr);
-        }
 	}
