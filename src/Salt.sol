8ec22dedf7194375d498b57ff6be7975de38252f
salty-io <125950061+salty-io@users.noreply.github.com>
2023-11-15 13:18:32 -0800

    Events added

--- src/Salt.sol
+++ src/Salt.sol
@@ -5,32 +5,34 @@ import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "./interfaces/ISalt.sol";
 
 
-contract SALT is ISalt, ERC20
+contract Salt is ISalt, ERC20
     {
+    event SALTBurned(uint256 amount);
+
 	uint256 public constant MILLION_ETHER = 1000000 ether;
 	uint256 public constant INITIAL_SUPPLY = 100 * MILLION_ETHER ;
 
 
 	constructor()
-		ERC20( "testSALT", "SALT" )
+		ERC20( "Salt", "SALT" )
 		{
 		_mint( msg.sender, INITIAL_SUPPLY );
         }
 
 
 	// SALT tokens will need to be sent here before they are burned.
-	// Normally there will be no SALT balance on this contract - only SALT to burn will be sent here.
-    function burnTokensInContract() public returns (uint256)
+	// There should otherwise be no SALT balance in this contract.
+    function burnTokensInContract() external
     	{
     	uint256 balance = balanceOf( address(this) );
     	_burn( address(this), balance );
 
-    	return balance;
+    	emit SALTBurned(balance);
     	}
 
 
     // === VIEWS ===
-    function totalBurned() public view returns (uint256)
+    function totalBurned() external view returns (uint256)
     	{
     	return INITIAL_SUPPLY - totalSupply();
     	}
