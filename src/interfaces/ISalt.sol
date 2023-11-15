f0d10c055649409449eb6c5481482161619d0b1e
salty-io <125950061+salty-io@users.noreply.github.com>
2023-11-15 13:36:29 -0800

    Burn amount returned

--- src/interfaces/ISalt.sol
+++ src/interfaces/ISalt.sol
@@ -6,7 +6,7 @@ import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 interface ISalt is IERC20
 	{
-	function burnTokensInContract() external returns (uint256);
+	function burnTokensInContract() external;
 
 	// Views
 	function totalBurned() external returns (uint256);
