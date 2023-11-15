2a0b75ec5c0ff59b9589383f635d23d9dbf014c3
salty-io <125950061+salty-io@users.noreply.github.com>
2023-11-15 13:39:55 -0800

    refactor with no more counterswap

--- src/root_tests/ITestUpkeep.sol
+++ src/root_tests/ITestUpkeep.sol
@@ -5,19 +5,14 @@ pragma solidity =0.8.22;
 interface ITestUpkeep
 	{
 	function step1() external;
-	function step2() external;
+	function step2( address receiver ) external;
 	function step3() external;
 	function step4() external;
-	function step5( address receiver ) external;
+	function step5() external;
 	function step6() external;
 	function step7() external;
 	function step8() external;
 	function step9() external;
-	function step10( uint256 daoStartingSaltBalance ) external;
-	function step11( uint256 timeSinceLastUpkeep ) external;
-	function step12( bytes32[] memory poolIDs) external;
-	function step13( uint256 timeSinceLastUpkeep ) external;
-	function step14() external;
-	function step15() external;
-	function step16() external;
+	function step10() external;
+	function step11() external;
 	}
